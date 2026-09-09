16b5065e1e282cc0905cb64013938df4db835357
Alonso678 <darkalon678@gmail.com>
2026-09-09 15:33:42 -0600

    feat(security): restringir endpoint de verificación de boletos a rol ADMIN
    * Se actualizó la configuración en `SecurityConfig.java` para proteger el endpoint `/api/sorteo/verificar`, asegurando que solo los usuarios con el rol `ADMIN` puedan acceder a la validación de autenticidad y estado de los boletos.

--- src/main/java/com/rifas/publicas/SecurityConfig.java
+++ src/main/java/com/rifas/publicas/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/registro", "/terminos-y-condiciones", "/aviso-de-privacidad", "/login","/reset-password", "/recuperar-password","/api/sugerencias/**", "/rifas/**", "/css/**", "/js/**", "/assets/**")
                         .permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/admin/**", "/api/sorteo/verificar").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                 .loginPage("/login")
